6a9ede19e3a11a407af9dc5af476fa652b58bcd1
Le Quoc Viet <vietlq85@gmail.com>
2018-05-07 02:59:43 +0100

    added tests for Mylist & CustomStack

--- naive-stack/example.ml
+++ naive-stack/example.ml
@@ -1,18 +1,23 @@
-open Mylist
-open Customstack
+module ML = Mylist.Mylist
 
-let cs = CustomStack.empty
+module CS = Customstack.CustomStack
 
-let cs1 = CustomStack.cons 1 cs ;;
-let cs2 = CustomStack.cons 2 cs1 ;;
+let cs = CS.empty
 
-assert (CustomStack.get 0 cs1 = 1) ;;
-assert (CustomStack.get 0 cs2 = 2) ;;
-assert (CustomStack.get 1 cs2 = 1) ;;
+let cs1 = CS.cons 1 cs ;;
+let cs2 = CS.cons 2 cs1 ;;
 
-(********)
+assert (CS.get 0 cs1 = 1) ;;
+assert (CS.get 0 cs2 = 2) ;;
+assert (CS.get 1 cs2 = 1) ;;
 
-let l = Mylist.empty
+(********************************)
 
-let l1 = Mylist.cons 1 l ;;
-let l2 = Mylist.cons 2 l1 ;;
+let l = ML.empty
+
+let l1 = ML.cons 1 l ;;
+let l2 = ML.cons 2 l1 ;;
+
+assert (ML.get 0 l1 = 1) ;;
+assert (ML.get 0 l2 = 2) ;;
+assert (ML.get 1 l2 = 1) ;;
